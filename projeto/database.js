9fc885a9171601ac39f77cd538d1ae2522235429
anacarolinacastello <anacarolinacastellop@gmail.com>
2021-07-11 18:54:38 -0300

    projeto

---
+++ projeto/database.js
@@ -0,0 +1,49 @@
+const livros = [
+  {
+    id: 1,
+    nome: 'O milagre do amanhã',
+    autor: 'Hal Erold',
+    categoria: 'Motivacional',
+    paginas: 196,
+    recomenda: true,
+    leu: false
+  },
+  {
+    id: 2,
+    nome: 'Arrume a sua cama',
+    autor: 'William McRaven',
+    categoria: 'Motivacional',
+    paginas: 128,
+    recomenda: true,
+    leu: true
+  },
+  {
+    id: 3,
+    nome: 'Confie em mim',
+    autor: 'Harlan Coben',
+    categoria: 'Suspense',
+    paginas: 440,
+    recomenda: true,
+    leu: true
+  },
+  {
+    id: 4,
+    nome: 'START',
+    autor: ' Jon Acuff',
+    categoria: 'Autoajuda',
+    paginas: 232,
+    recomenda: false,
+    leu: true
+  },
+  {
+    id: 5,
+    nome: 'A Seleção - Vol. 2 ',
+    autor: 'Kiera Cass',
+    categoria: 'Atemporal',
+    paginas: 360,
+    recomenda: false,
+    leu: false
+  }
+]
+
+module.exports = livros
